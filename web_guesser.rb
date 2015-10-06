51a6af2ff45f1566f45ed74b03086d47d9219af7
acareaga <aaroncareaga@gmail.com>
2015-10-06 10:26:05 -0600

    Refactored and cleaned up files.

--- web_guesser.rb
+++ web_guesser.rb
@@ -12,11 +12,11 @@ get '/' do
 end
 
 def check_guess(guess)
-  if guess >= SECRET_NUMBER + 5
+  if guess >= SECRET_NUMBER + 10
     message = "Way too high!"
   elsif guess > SECRET_NUMBER
     message = "Too high!"
-  elsif guess <= SECRET_NUMBER - 5
+  elsif guess <= SECRET_NUMBER - 10
     message = "Way too low!"
   elsif guess < SECRET_NUMBER
     message = "Too low!"
